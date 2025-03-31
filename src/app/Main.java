bea0780e35c554293f272251eb02b108f89c4164
theiann <2015iann@gmail.com>
2025-03-31 13:18:00 -0500

    Implemented MidiEventData object

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,23 @@
 package app;
 
+import java.util.List;
+
+import javax.sound.midi.*;
+import file_reader.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("./app.files/mystery_song.csv");
+			Sequence sequence = new Sequence(Sequence.PPQ, 384);
+			Track track = sequence.createTrack();
+			
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
