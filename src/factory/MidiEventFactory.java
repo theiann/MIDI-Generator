b179b0ae2f778d91535fc49b956e53d7544c2c96
theiann <2015iann@gmail.com>
2025-03-31 19:46:45 -0500

    Implemented MidiEventFactory interface

--- src/factory/MidiEventFactory.java
+++ src/factory/MidiEventFactory.java
@@ -1,5 +1,9 @@
 package factory;
 
-public interface MidiEventFactory {
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
 
+public interface MidiEventFactory {
+	MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
+	MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 }
