8977146d1f9a4f5d7e1bcfe15b16d24a7cddcd12
jibes <jibes@Jibesh_PC>
2025-04-02 15:45:54 -0500

    Finished trumpet instrument strategy

--- src/instrument/TrumpetStrategy.java
+++ src/instrument/TrumpetStrategy.java
@@ -2,12 +2,21 @@ package instrument;
 
 import javax.sound.midi.*;
 
-public class TrumpetStrategy implements InstrumentStrategy {
+public class TrumpetStrategy implements InstrumentStrategy {/*
+	 * This class applies the Trumpet Instrument to the channel
+	 * It does so with the MIDI program 56
+	 */
+		@Override
+		public void applyInstrument(Track track, int channel) {
+			try {
+				ShortMessage addingInstrument = new ShortMessage(); 
+				addingInstrument.setMessage(ShortMessage.PROGRAM_CHANGE, channel,56,0);
+				track.add(new MidiEvent(addingInstrument,0));
+			}
+			catch(InvalidMidiDataException e){
+				System.out.println("Could not apply Trumpet Instrument");
+				
+			}
 
-	@Override
-	public void applyInstrument(Track track, int channel) {
-		// TODO Auto-generated method stub
-
-	}
-
+		}
 }
