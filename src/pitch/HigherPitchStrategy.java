bcc711303922ffe688dff0e02fbbb6bc0caa8d8b
jibes <jibes@Jibesh_PC>
2025-04-02 14:54:48 -0500

    completed the higherPitchStrategy and LowerePitchStrategy classes

--- src/pitch/HigherPitchStrategy.java
+++ src/pitch/HigherPitchStrategy.java
@@ -1,11 +1,14 @@
 package pitch;
 
 public class HigherPitchStrategy implements PitchStrategy {
-
+	/*
+	 * This implements the pitch strategy interface and it increases the pitch 
+	 * volume by 2 if called. If passed note value is 127 or 126, it would just return
+	 * 127, since that is set as the maximum
+	 */
 	@Override
 	public int modifyPitch(int note) {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.max(note+2,127); 
 	}
 
 }
