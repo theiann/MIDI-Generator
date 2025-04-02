d96a1ccf49375b4f628a7f58091106d118afa485
jibes <jibes@Jibesh_PC>
2025-04-02 15:02:48 -0500

    Completed the HigherPitchStrategy and LowerPitchStrategy classes

--- src/pitch/LowerPitchStrategy.java
+++ src/pitch/LowerPitchStrategy.java
@@ -1,11 +1,14 @@
 package pitch;
 
 public class LowerPitchStrategy implements PitchStrategy {
-
-	@Override
+/*
+ * This implements the pitch strategy interface and it reduces the pitch 
+ * volume by 2 if called. If passed note value is 0 or 0, it would just return
+ * 0, since that is set as the minimum
+ */
+	@Override   
 	public int modifyPitch(int note) {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.min(note - 2,0); 
 	}
 
 }
