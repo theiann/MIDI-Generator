549c61f60cb33cba005dcc2f586597861f8c4044
jibes <jibes@Jibesh_PC>
2025-04-02 15:40:48 -0500

    Completed the AcousticGrandPianoStrategy and ElectricBassGuitarPiano Strategy

--- src/instrument/AcousticGrandPianoStrategy.java
+++ src/instrument/AcousticGrandPianoStrategy.java
@@ -3,10 +3,21 @@ package instrument;
 import javax.sound.midi.*;
 
 public class AcousticGrandPianoStrategy implements InstrumentStrategy {
-
+/*
+ * This class applies the acoustic Grand Piano Instrument to the channel
+ * It does so with the MIDI program 0
+ */
 	@Override
 	public void applyInstrument(Track track, int channel) {
-		// TODO Auto-generated method stub
+		try {
+			ShortMessage addingInstrument = new ShortMessage(); 
+			addingInstrument.setMessage(ShortMessage.PROGRAM_CHANGE, channel,0,0);
+			track.add(new MidiEvent(addingInstrument,0));
+		}
+		catch(InvalidMidiDataException e){
+			System.out.println("Could not apply Piano Instrument");
+			
+		}
 
 	}
 
--- src/instrument/ElectricBassGuitarStrategy.java
+++ src/instrument/ElectricBassGuitarStrategy.java
@@ -3,11 +3,21 @@ package instrument;
 import javax.sound.midi.*;
 
 public class ElectricBassGuitarStrategy implements InstrumentStrategy {
+	/*
+	 * This class applies the Electric bass guitar Instrument to the channel
+	 * It does so with the MIDI program 33
+	 */
+		@Override
+		public void applyInstrument(Track track, int channel) {
+			try {
+				ShortMessage addingInstrument = new ShortMessage(); 
+				addingInstrument.setMessage(ShortMessage.PROGRAM_CHANGE, channel,33,0);
+				track.add(new MidiEvent(addingInstrument,0));
+			}
+			catch(InvalidMidiDataException e){
+				System.out.println("Could not apply Guitar Instrument");
+				
+			}
 
-	@Override
-	public void applyInstrument(Track track, int channel) {
-		// TODO Auto-generated method stub
-
-	}
-
+		}
 }
